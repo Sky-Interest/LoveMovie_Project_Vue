ad1f366da0655bad1a28ffca37f39d0f352d5e64
Sky_Interest <841102500@qq.com>
2024-09-20 09:56:19 +0800

    6.角色管理开发 管理列表开发 编写role.js

---
+++ src/api/role/role.js
@@ -0,0 +1,14 @@
+import request from '@/utils/request'
+
+const BASE_URL = '/admin/system/role';
+export default {
+    // 列表 
+    getPageList(page, limit, searchObj) {
+        return request({
+            // url: '/admin/system/role' + page + "/" + limit,
+            url: `${BASE_URL}/${page}/${limit}`,
+            method: 'get',
+            params: searchObj
+        })
+    }
+}
