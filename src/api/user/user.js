f73a8d0b4c24d8ce3e107189a693c959c3b9a0e2
Sky_Interest <841102500@qq.com>
2024-09-20 21:12:15 +0800

    7.3.1用户管理功能开发 添加用户功能 编写api

--- src/api/user/user.js
+++ src/api/user/user.js
@@ -3,13 +3,21 @@ import request from '@/utils/request'
 // 定义公共的api 
 const BASE_URL = '/admin/system/sysUser';
 
-export default{
+export default {
     // 查询用户和分页
-    getUserPageInfo(page,limit,searchObj){
+    getUserPageInfo(page, limit, searchObj) {
         return request({
-          url:`${BASE_URL}/${page}/${limit}`,
-          method: 'get',
-          params:searchObj
+            url: `${BASE_URL}/${page}/${limit}`,
+            method: 'get',
+            params: searchObj
         })
-      },
+    },
+    // 添加用户
+    saveUser(user) {
+        return request({
+            url: `${BASE_URL}/addUser`,
+            method: 'post',
+            data: user
+        })
+    },
 }
